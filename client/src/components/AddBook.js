c6b6e8ece0838f0e9d56f2f821b8ea357f0032b6
Gabriel Oliveira Martins <gabriel.oliveiragom@gmail.com>
2018-06-27 12:46:43 +0200

    getting the details of the books

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import { getAuthorsQuery } from '../queries/queries'
+import { graphql, compose } from 'react-apollo';
+import { getAuthorsQuery, addBookMutation, getBooksQuery } from '../queries/queries'
 
 class AddBook extends Component {
     constructor(props){
@@ -12,7 +12,7 @@ class AddBook extends Component {
         };
     }
     displayAuthors(){
-        var data = this.props.data;
+        var data = this.props.getAuthorsQuery;
         if(data.loading){
             return(<option disabled>Loading Authors...</option>);
         }
@@ -24,7 +24,14 @@ class AddBook extends Component {
     }
     submitForm(e){
         e.preventDefault();
-        console.log(this.state);
+        this.props.addBookMutation({
+            variables: {
+                name: this.state.name,
+                genre: this.state.genre,
+                authorId: this.state.authorId
+            },
+            refetchQueries: [{ query: getBooksQuery }]
+        });
     }
     render() {
         return (
@@ -55,4 +62,7 @@ class AddBook extends Component {
     }
 }
 
-export default graphql(getAuthorsQuery)(AddBook);
+export default compose(
+    graphql(getAuthorsQuery, { name: "getAuthorsQuery" }),
+    graphql(addBookMutation, { name: "addBookMutation" })
+)(AddBook);
